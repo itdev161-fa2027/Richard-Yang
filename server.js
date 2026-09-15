9fa90ec88b540ca7790ce299a461e3d767c1ed77
yangr6 <yangr6@matc.edu>
2026-09-15 14:08:26 -0500

    Create User Registration API Endpoint

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
 
+//initialize express app
 const app = express();
 
 connectDatabase();
 
+//Middleware
+app.use(express.json());
+
 app.get('/',(req,res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/user
+ * @desc Register user
+ */
+app.post('/api/users',(req, res) =>{
+    console.log(req,body);
+    res.send(req.body);
+});
+
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
 
